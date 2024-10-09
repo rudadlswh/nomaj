20cd23e9f550dc6eaef2abfdea81c59ad43cc51e
whrudals <96468366+rudadlswh@users.noreply.github.com>
2024-10-09 16:27:07 +0900

    1

---
+++ src/main/java/com/t/nomaj/NomajApplication.java
@@ -0,0 +1,13 @@
+package com.t.nomaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t/nomaj/NomajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t.nomaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
